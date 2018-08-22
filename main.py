95a6936df5b5c5d9ed53d342b24774f4b89e3cc0
tuninx <tuninx@gmail.com>
2018-08-22 04:31:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -117,9 +117,9 @@ def parse_text(text, username, message_id):
         log('Получили сообщение от бота. Проверяем условия')
 
         if corovan_enabled and text.find(' пытается ограбить') != -1:
-        sleep_time = random.randint(2, 5)
-        sleep(sleep_time)
-            action_list.append(orders['corovan'])
+            sleep_time = random.randint(2, 5)
+            sleep(sleep_time)
+                action_list.append(orders['corovan'])
             
 
 def mark_read(bot_username):
