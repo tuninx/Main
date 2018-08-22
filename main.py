cac6cc75bb54e7dc45feea545660c2d41c459e7c
tuninx <tuninx@gmail.com>
2018-08-22 20:20:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def parse_text(text, username, message_id):
             sleep_time = random.randint(2, 15)
             sleep(sleep_time)
             action_list.append(orders['corovan'])
-            mark_read(bot_username)
+            mark_read('@', bot_username)
             
 def mark_read(pref, to):
     sender.mark_read(pref + to)
