cee58508c4dbe18132f575883a001dcbc5452866
tuninx <tuninx@gmail.com>
2018-08-22 04:07:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -120,8 +120,8 @@ def parse_text(text, username, message_id):
             action_list.append(orders['corovan'])
             mark_read(bot_username)
 
-def mark_read(self, message):
-    sender.mark_read(self, message)
+def mark_read(self, message_id):
+    sender.mark_read(self, message_id)
             
           
 def send_msg(pref, to, message):
